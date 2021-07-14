e3e43ea6f9235afb66c4ddf15d8b6baa6f21512f
theruziev2004 <ruzievrustam58@yandex.ru>
2021-07-14 20:06:13 +0500

    done home work

--- main.go
+++ main.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Rustam")
+	minAmount := 5000
+	purchase := 15000
+	limitCashback := 2000
+	percentCashback := 15
+	cashback := 0
+	const fullPercent = 100
+
+	if purchase >= minAmount {
+		cashback = purchase * percentCashback / fullPercent
+		fmt.Println("Prediction cashback is", cashback)
+	}
+
+	if limitCashback > cashback {
+		cashback = limitCashback
+		fmt.Println("cashback is", cashback)
+	}
+	//----HOME WORK----
+	// cashback
+	// Если сумма покупки состовляет более 500смн скидка в размере 15%
+	// Если сумма кешбека превышает 2000 смн, то кешбек должен быть 2000смн
 }
